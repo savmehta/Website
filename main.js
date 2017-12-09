9dda744895a7daa259f13862f44bc130eb0a714b
Savan Mehta <P15211562@my365.dmu.ac.uk>
2017-12-09 21:54:34 +0000

    VIsual and text content change
    Content box background colour changed to transparent black in CSS stylesheet, text content added to index page content boxes

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function carousel() {
     slideIndex++;
     if (slideIndex > x.length) {slideIndex = 1}
     x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 5000); // Change image every 2 seconds
+    setTimeout(carousel, 3000); // Change image every 2 seconds
 }
     var slideIndex = 1;
 showDivs(slideIndex);
